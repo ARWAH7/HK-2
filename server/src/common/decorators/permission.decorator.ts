c7afb223122cd93d4f5488c8bec6440c165ab006
Woli <45162978+ARWAH7@users.noreply.github.com>
2026-04-08 10:07:03 +0800

    Delete server directory

--- server/src/common/decorators/permission.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const PERMISSION_KEY = 'permission_code';
-export const RequirePermission = (code: string) => SetMetadata(PERMISSION_KEY, code);
